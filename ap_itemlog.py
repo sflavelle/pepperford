dd916988efd887ede05cf975b7f3cc9afea4d0ff
Lily Flavelle <me@neurario.com>
2025-02-22 14:47:03 +1100

    facepalm

--- ap_itemlog.py
+++ ap_itemlog.py
@@ -213,7 +213,7 @@ def process_new_log_lines(new_lines, skip_msg: bool = False):
             timestamp, sender = match.groups()
             if sender not in players: players[sender] = {"goaled": True}
             players[sender].goaled = True
-            message = f"**{sender} has finished!** That's {len([p for p in players if p.is_goaled()])}/{len(players)} done!"
+            message = f"**{sender} has finished!** That's {len([p for p in players.values() if p.is_goaled()])}/{len(players)} done!"
             if not skip_msg: message_buffer.append(message)
         elif match := regex_patterns['releases'].match(line):
             timestamp, sender = match.groups()
