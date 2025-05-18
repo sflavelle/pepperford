09fe24cfd540e53c8863778795fb0dc9832a018b
Lily Flavelle <me@neurario.com>
2025-05-19 01:03:16 +1000

    Slightly different formatting for your own hints

--- cmds/archipelago.py
+++ cmds/archipelago.py
@@ -433,11 +433,14 @@ class Archipelago(commands.GroupCog, group_name="archipelago"):
         hints_list = "## To Find:"
         for hint in hint_table_list:
             if hint["Sender"] not in linked_slots: continue
+            if hint["Sender"] == hint["Receiver"]:
+                hints_list += f"\n**Your {hint['Item']}** is on {hint['Location']}{f" at {hint['Entrance']}" if hint['Entrance'] else ""}."
             hints_list += f"\n**{hint['Receiver']}'s {hint['Item']}** is on {hint['Location']}{f" at {hint['Entrance']}" if hint['Entrance'] else ""}."
 
         hints_list += "\n\n## To Be Found:"
         for hint in hint_table_list:
             if hint["Receiver"] not in linked_slots: continue
+            if hint["Sender"] == hint["Receiver"]: continue
             hints_list += f"\n**Your {hint['Item']}** is on {hint['Sender']}'s {hint['Location']}{f" at {hint['Entrance']}" if hint['Entrance'] else ""}."
 
 
