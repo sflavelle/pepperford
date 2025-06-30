174ce244377442b13bacb97ce46749a52d07d8c2
Lily Flavelle <me@neurario.com>
2025-07-01 00:56:39 +1000

    itemlog/tracking: I don't like this. but we perservere

--- cmds/ap_scripts/utils.py
+++ cmds/ap_scripts/utils.py
@@ -683,7 +683,12 @@ def handle_item_tracking(game: Game, player: Player, item: Item):
                     return item.replace("_", " ").replace("-"," - ")
                 case "Jigsaw":
                     if item.endswith("Puzzle Pieces"):
-                        starting_pieces_item = next((item for item in player.inventory if item.location == "Starting Items"), None)
+                        starting_pieces_item = None
+                        for item in player.inventory:
+                            if item.location == "Starting Items" and item.sender == "Archipelago":
+                                if item.name.endswith("Puzzle Pieces"):
+                                    starting_pieces_item = item
+                                    break
 
                         starting_pieces: int = int(starting_pieces_item.name.split()[0]) if starting_pieces_item else 0
                         pieces_per_item: int = int(item.split()[0])
