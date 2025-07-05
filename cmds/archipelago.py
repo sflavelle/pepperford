cb64d87c1a0e43e21d80ed8418e978952e685703
Lily Flavelle <me@neurario.com>
2025-07-06 00:44:51 +1000

    ap/status: note if epople are *currently* online

--- cmds/archipelago.py
+++ cmds/archipelago.py
@@ -534,7 +534,7 @@ class Archipelago(commands.GroupCog, group_name="archipelago"):
         msg_lines.append("")
 
         for player in game_table['players'].values():
-            last_online = lambda player: f"Last online <t:{int(player['last_online'])}:R>." if player['last_online'] is not None else "Never logged in."
+            last_online = lambda player: f"Last online <t:{int(player['last_online'])}:R>." if player['last_online'] is not None else "Online right now." if player['online'] is True else "Never logged in."
             if player['goaled'] is True:
                 msg_lines.append(f"- **{player['name']} ({player['game']})**: finished their game with {round(player['collection_percentage'], 2)}% checks collected. {last_online(player)}")
             elif player['released'] is True and player['goaled'] is False:
