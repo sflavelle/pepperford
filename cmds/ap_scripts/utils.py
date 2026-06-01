af15dcb56feb13e9637c4d43a62fa4532d394d74
Lily Flavelle <me@neurario.com>
2026-06-01 12:16:02 +1000

    itemlog/tracking/miuu: update next chapter pointer

--- cmds/ap_scripts/utils.py
+++ cmds/ap_scripts/utils.py
@@ -2020,15 +2020,19 @@ def handle_item_tracking(game: Game, player: Player, item: Item):
                         next_required: int
                         current_chapter: int
                         current_chapter_str: str
+                        next_chapter: int
+                        next_chapter_str: str
                         for idx, chapter in enumerate(chapters):
                             next_required = (idx * medals_per_unlock) + 5
                             if count >= next_required:
                                 continue
                             elif count < next_required:
-                                current_chapter = idx - 1
-                                current_chapter_str = chapters[idx]
+                                current_chapter = idx
+                                next_chapter = idx + 1
+                                current_chapter_str = chapters[current_chapter]
+                                next_chapter_str = chapters[next_chapter]
                                 break
-                        next_required_string = f" ({count}/{next_required} to {chapters[idx]})" if current_chapter < len(chapters)-1 else ""
+                        next_required_string = f" ({count}/{next_required} to {next_chapter_str})" if current_chapter < len(chapters)-1 else ""
                         return f"{item} (*{count}/{goal_required}*){next_required_string}"
 
                 case "Mega Man 2":
